0c9215d4aa8bcf147dc8d113b28a6608bd12520c
Pavlo Kapyshin <i@93z.org>
2016-12-23 16:20:38 +0100

    Add classifiers & keywords

--- setup.py
+++ setup.py
@@ -9,6 +9,15 @@ setuptools.setup(
     namespace_packages=["paka"],
     zip_safe=False,
     url="https://github.com/PavloKapyshin/paka.funcreg",
+    keywords="registry",
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.5"],
     license="BSD",
     author="Pavlo Kapyshin",
     author_email="i@93z.org")
